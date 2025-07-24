eb2feac659d00686e5c93daeb216008f13e7ac15
UNIGO777 <naman13399@gmail.com>
2025-07-24 22:46:04 +0530

    akdjf

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,10 @@ import CA from './Pages/CA'
 import Footer from './Components/Footer'
 import AmvikaSoftware from './Pages/AmbikaSoftware/Index'
 import GrowWith from './Pages/GrowWithUs'
-import AboutUs from './Pages/AboutUs'
+import AboutUs from './Pages/AboutUS'
 import ContactUs from './Pages/ContactPage'
 import Website from './Pages/WebsiteDevelopment'
-import OurFrancise from './Pages/AboutUs/sections/OurFrancises'
+import OurFrancise from './Pages/AboutUS/sections/OurFrancises.jsx'
 
 const App = () => {
   return (
